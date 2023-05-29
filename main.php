575c1e3807846dd562f3f1810e0c86486df3935b
Parsyfall <72997097+Parsyfall@users.noreply.github.com>
2023-05-29 18:07:03 +0300

    remov comments
    Removed some commented blocks and ideas

--- main.php
+++ main.php
@@ -50,12 +50,8 @@ require('php/mysql.php');
 
 
     </div>
-    <!-- <form action="book.php?id=5" method="post">
-            <button type="submit">Click</button>
-        </form> -->
 
     <?php require('./php/footer.php'); ?>
-    <!-- Latest books, published within a year -->
 </body>
 
 </html>
